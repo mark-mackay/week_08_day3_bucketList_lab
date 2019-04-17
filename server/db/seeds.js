35fb55324fea0c4577ad45b88bcdf359fefc6be4
Mark Mackay <Mark_Mackay@yahoo.com>
2019-04-17 13:23:48 +0100

    post-it note style added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,36 +5,76 @@ db.bucketlists.insertMany([
   {
     item: "Fly Helicopter",
     description: "Get a helicopter flying lesson or two",
-    priotity: "Urgent"
+    priority: "Urgent"
   },
   {
     item: "Mow Lawn",
     description: "Mow lawn in Iceland at midnight(not the shop)",
-    priotity: "Soon"
+    priority: "Soon"
   },
   {
     item: "Terraform Planet",
     description: "Terraform any planet before earth is goosed",
-    priotity: "Urgent"
+    priority: "Urgent"
   },
   {
     item: "Own a Tesla",
     description: "Delivered by Elon himself",
-    priotity: "Urgent"
+    priority: "Urgent"
   },
   {
     item: "Swim with dolphins",
     description: "Not at a Seaworld",
-    priotity: "Non-Urgent"
+    priority: "Non-Urgent"
   },
   {
     item: "Enter Zero Gravity",
     description: "Whatever it takes",
-    priotity: "Soon"
+    priority: "Soon"
   },
   {
     item: "Eat the rich",
     description: "Before it is too late",
-    priotity: "Urgent"
-  }
+    priority: "Urgent"
+  },
+  {
+    item: "Fly Helicopter",
+    description: "Get a helicopter flying lesson or two",
+    priority: "Urgent"
+  },
+  {
+    item: "Mow Lawn",
+    description: "Mow lawn in Iceland at midnight(not the shop)",
+    priority: "Soon"
+  },
+  {
+    item: "Terraform Planet",
+    description: "Terraform any planet before earth is goosed",
+    priority: "Urgent"
+  },
+{
+  item: "Own a Tesla",
+  description: "Delivered by Elon himself",
+  priority: "Urgent"
+},
+{
+  item: "Enter Zero Gravity",
+  description: "Whatever it takes",
+  priority: "Soon"
+},
+{
+  item: "Eat the rich",
+  description: "Before it is too late",
+  priority: "Urgent"
+},
+{
+  item: "Something Really Daft",
+  description: "Not at home",
+  priority: "Urgent"
+},
+{
+  item: "Swim with dolphins",
+  description: "Not at a Seaworld",
+  priority: "Non-Urgent"
+}
 ]);
